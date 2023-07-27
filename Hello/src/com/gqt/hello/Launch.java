1262989f9dbbfa81b298b01cce967c971f98728f
Kbharath123 <kammaribharath225@email.com>
2023-07-27 10:51:00 +0530

    first commit

---
+++ Hello/src/com/gqt/hello/Launch.java
@@ -0,0 +1,14 @@
+package com.gqt.hello;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hi");
+		System.out.println("Bharath123");
+		System.out.println("Bye");
+	}
+
+}
